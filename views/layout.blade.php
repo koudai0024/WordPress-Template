20311554f97cf53299e0f4586195e81cac7e3df4
koudai0024 <iwai.0731v@gmail.com>
2021-09-05 14:09:02 +0900

    ✨ wordpress用の記述追加

--- views/layout.blade.php
+++ views/layout.blade.php
@@ -2,19 +2,21 @@
 <html lang="ja">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ bloginfo('name') }}</title>
-    <link rel="stylesheet" href="{{ get_template_directory_uri() }}/style.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>{{ bloginfo('name') }}</title>
+	<link rel="stylesheet" href="{{ get_template_directory_uri() }}/style.css">
+	@php wp_head(); @endphp
 </head>
 
 <body>
-    @include('header')
-    <main>
-        @yield('content')
-    </main>
-    @include('footer')
+	@include('header')
+	<main>
+		@yield('content')
+	</main>
+	@include('footer')
+	@php wp_footer(); @endphp
 </body>
 
 </html>
